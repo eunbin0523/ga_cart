89fe2ae27040803fe50f9fa379fd66d5c23e950f
eunbin0523 <silverbin0523@naver.com>
2023-12-08 13:02:54 +0000

    sign up success

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,76 +1,69 @@
 package com.example.demo.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.example.demo.service.UserDetailsServiceImpl;
 
 @Configuration
-@EnableTransactionManagement
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	   UserDetailsServiceImpl userDetailsService;
-	 
-	   @Bean
-	   public BCryptPasswordEncoder passwordEncoder() {
-	      BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-	      return bCryptPasswordEncoder;
-	   }
-	 
-	   @Autowired
-	   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	 
-	      // Setting Service to find User in the database.
-	      // And Setting PassswordEncoder
-	      auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	 
-	   }
-	 
-	   @Override
-	   protected void configure(HttpSecurity http) throws Exception {
-	 
-	      http.csrf().disable();
 
-		  // 회원가입 페이지는 모든 사용자에게 허용
-		  http.authorizeRequests().antMatchers("/admin/login", "/admin/signup").permitAll();
-	 
-	      // Requires login with role ROLE_EMPLOYEE or ROLE_MANAGER.
-	      // If not, it will redirect to /admin/login.
-	      http.authorizeRequests().antMatchers("/admin/orderList", "/admin/order", "/admin/accountInfo")//
-	            .access("hasAnyRole('ROLE_EMPLOYEE', 'ROLE_MANAGER')");
-	 
-	      // Pages only for MANAGER
-	      http.authorizeRequests().antMatchers("/admin/product").access("hasRole('ROLE_MANAGER')");
-	 
-	      // When user login, role XX.
-	      // But access to the page requires the YY role,
-	      // An AccessDeniedException will be thrown.
-	      http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-	 
-	      // Configuration for Login Form.
-	      http.authorizeRequests().and().formLogin()//
-	            .loginProcessingUrl("/login") // Submit URL
-	            .loginPage("/admin/login")//
-	            .defaultSuccessUrl("/admin/login?success=true")//
-	            .failureUrl("/admin/login?error=true")//
-	            .usernameParameter("userName")//
-	            .passwordParameter("password")
-	 
-	            // Configuration for the Logout page.
-	            // (After logout, go to home page)
-	            .and().logout().logoutUrl("/admin/logout").logoutSuccessUrl("/");
-		// 로그인 페이지의 POST 요청 처리 URL 설정
-		http.formLogin().loginProcessingUrl("/j_spring_security_check");
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-		// 이 부분이 추가된 부분입니다. 
-		http.formLogin().permitAll();
-	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // Setting Service to find User in the database.
+        // And Setting PasswordEncoder
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/admin/login", "/admin/signup").permitAll();
+	    // Requires login with role ROLE_EMPLOYEE or ROLE_MANAGER.
+	    // If not, it will redirect to /admin/login.
+	    http.authorizeRequests().antMatchers("/admin/orderList", "/admin/order", "/admin/accountInfo")//
+	        .access("hasAnyRole('ROLE_EMPLOYEE', 'ROLE_MANAGER')");
+	 
+	    // Pages only for MANAGER
+	    http.authorizeRequests().antMatchers("/admin/product").access("hasRole('ROLE_MANAGER')");
+	 
+	    // When user login, role XX.
+	    // But access to the page requires the YY role,
+	    // An AccessDeniedException will be thrown.
+	    http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+	 
+	    // Configuration for Login Form.
+	    http.authorizeRequests().and().formLogin()//
+	 
+	            //
+	        .loginProcessingUrl("/j_spring_security_check") // Submit URL
+	        .loginPage("/admin/login")//
+	        .defaultSuccessUrl("/admin/accountInfo")//
+	        .failureUrl("/admin/login?error=true")//
+	        .usernameParameter("userName")//
+	        .passwordParameter("password")
+	 
+	        // Configuration for the Logout page.
+	        // (After logout, go to home page)
+	        .and().logout().logoutUrl("/admin/logout").logoutSuccessUrl("/");
+	   }
 }
--- src/main/java/com/example/demo/dao/AccountDAO.java
+++ src/main/java/com/example/demo/dao/AccountDAO.java
@@ -4,15 +4,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.Account;
 
 @Repository
+@Transactional
 public class AccountDAO {
  
     @Autowired
     private SessionFactory sessionFactory;
  
+    @Transactional(readOnly = true)
     public Account findAccount(String userName) {
         Session session = this.sessionFactory.getCurrentSession();
         return session.find(Account.class, userName);
@@ -23,3 +26,4 @@ public class AccountDAO {
         session.persist(account);
     }
 }
+
